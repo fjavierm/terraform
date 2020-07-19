b67f93aa4e01a778c39815e4a27d3f8ac3c30247
fjavierm <fco.javier.marquez@gmail.com>
2020-07-19 09:49:05 +0100

    Create first EC2 instance

---
+++ project_01/main.tf
@@ -0,0 +1,10 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 2.70" # Optional
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "project_01_server" {
+  ami           = "ami-099926fbf83aa61ed"
+  instance_type = "t2.micro"
+}
